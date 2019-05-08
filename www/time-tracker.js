154a1cba6e397bf4c23874e19620293fa659e584
David McPike <7037366+mcdafydd@users.noreply.github.com>
2019-05-08 15:22:43 -0500

    UI updates
    - Fixes dark default theme
    - Cosmetic updates
    - Progressive web app changes to manifest, icons

--- www/time-tracker.js
+++ www/time-tracker.js
@@ -10,7 +10,9 @@ import {WiredToggle} from 'wired-toggle';
 class TimeTracker extends LitElement {
   static get properties() {
     return {
-      showHelp: { type: Boolean }
+      showHelp: { type: Boolean },
+      outputClass: { type: String },
+      outputText: { type: String }
     };
   }
 
@@ -49,11 +51,11 @@ class TimeTracker extends LitElement {
     // Return the template using the html template tag. This will allow lit-html to
     // interpret the dynamic parts of your template.
     return html`
-      <div><wired-input type="text" autofocus id="text-input" class="form-control text-input" name="command" @keyup="${this.handleInput}"></wired-input>
-      <div>
-        <wired-toggle id="helpme" class="toggle" @change="${this.doToggle}"></wired-toggle></wired-toggle>
+      <div class="user-input">
+        <wired-input type="text" autofocus id="text-input" class="form-control text-input" name="command" @keyup="${this.handleInput}"></wired-input>
       </div>
-      <span class="${this.outputClass}">${this.outputText}</span>
+      <div><wired-toggle id="helpme" class="toggle" @change="${this.doToggle}" ?checked=${this.showHelp}></wired-toggle></wired-toggle></div>
+      <div class="${this.outputClass}">${this.outputText}</div>
       <div id="helpText" ?hidden=${!this.showHelp}>${this.getHelpText()}</div>
     `;
   }
@@ -62,62 +64,97 @@ class TimeTracker extends LitElement {
     this.showHelp = !this.showHelp;
   }
 
-  // handleInput process user input
-  handleInput(e) {
-   if (e.key === 'Enter') {
-    // Cancel the default action, if needed
-    e.preventDefault();
-    var el = this.shadowRoot.getElementById('text-input');
-    var cmd = el.value.match(/([^harslebt]+)\s+([^\sA-z0-9_*-;]+)/g);
-    if (cmd === null) {
-      updateOutput('Invalid characters - alphanumeric only', 'red');
-    }
+  // handleCommand process user input and hide window after handling command without error
+  handleCommand(el, input) {
     var d = new Date();
-    console.log(d.toISOString(), ': Command entered = ', cmd);
-    handleCommand(cmd);
+    console.log(d.toISOString(), ': Command entered = ', input);
     // clear textarea for next command
-    this.value = '';
+    el.value = '';
+
+    var argv = input.split(/\s/);
+    var cmd = argv.shift();
     switch(cmd) {
       case 'h':
+        runUtt(['hello']);
+        minimize();
         break;
       case 'a':
+        if (argv.length > 0) {
+          runUtt(['add'].concat(argv));
+          minimize();  
+        }
+        else {
+          this.updateOutput('Add command requires task description', 'red');
+        }
         break;
       case 'r':
+        runUtt(['report', '--from', 'monday', '--to', 'friday']);
+        minimize();
         break;
       case 's':
+        runUtt(['stretch']);
+        minimize();
         break;
       case 'l':
+        runUtt(['report', '--from', 'monday', '--to', 'friday']);
+        minimize();
         break;
       case 'e':
+        runUtt(['edit']);
+        minimize();
         break;
       case 'b':
+        runUtt(['add', 'break', '**']);
+        minimize();
         break;
       case 't':
+        this.doToggle();
+        break;
+      case '?':
+        this.doToggle();
         break;
       default:
-        break;
+        this.updateOutput('Invalid command - try again or ? for help', 'red');
     }
   }
-}
+
+  // handleInput process user input, ensure the text entered is valid
+  handleInput(e) {
+    if (e.key === 'Enter') {
+      // Cancel the default action, if needed
+      e.preventDefault();
+      var el = this.shadowRoot.getElementById('text-input');
+      var cmd = el.value.match(/^([A-z?]+)(\s+[A-z0-9_*-;\s]+)?$/g);    
+      if (cmd === null) {
+        this.updateOutput('Invalid characters - alphanumeric only', 'red');
+        el.value = '';
+      }
+      else {
+        this.handleCommand(el, cmd[0]);  
+        // Clear any previous output
+        this.updateOutput('', 'black');
+      }
+    }
+  }
 
   updateOutput(data, color) {
-    this.outputColor = color;
+    this.outputClass = color;
     this.outputText = data;
   }
 
   getHelpText() {
     return html`
         <wired-listbox>
-        <wired-item value="cmdHello">hello (h) - start day</wired-item>
-        <wired-item value="cmdAdd">add (a) &lt;task&gt; - add &lt;task&gt; entry with current time (use at end of task, not beginning)</wired-item>
-        <wired-item value="cmdAddBreak">add (a) &lt;task&gt; ** - add break &lt;task&gt; entry with current time (ie: a break ***) (use at end of task, not beginning)</wired-item>
-        <wired-item value="cmdAddIgnore">add (a) &lt;task&gt;*** - add ignored &lt;task&gt; entry with current time (ie: a commuting ***) (use at end of task, not beginning)</wired-item>
-        <wired-item value="cmdReport">report (r) &lt;task&gt;*** - display this week\'s time report')</wired-item>
-        <wired-item value="cmdLast">last (l) - display last week\'s time report</wired-item>
-        <wired-item value="cmdStretch">stretch (s) &lt;task&gt;*** - stretch last task to current time')</wired-item>
-        <wired-item value="cmdEdit">edit (e) - edit current timesheet</wired-item>
-        <wired-item value="cmdBreak">break (b) - shortcut to add break **</wired-item>
-        <wired-item value="cmdToggle">toggle (t) - toggle this help text display</wired-item>`
+        <wired-item value="cmdHello">h (hello) - start day</wired-item>
+        <wired-item value="cmdAdd">a (add) &lt;task&gt; - add &lt;task&gt; entry with current time (use at end of task, not beginning)</wired-item>
+        <wired-item value="cmdAddBreak">a (add) &lt;task&gt; ** - add break &lt;task&gt; entry with current time (ie: a break ***) (use at end of task, not beginning)</wired-item>
+        <wired-item value="cmdAddIgnore">a (add) &lt;task&gt;*** - add ignored &lt;task&gt; entry with current time (ie: a commuting ***) (use at end of task, not beginning)</wired-item>
+        <wired-item value="cmdReport">r (report) &lt;task&gt;*** - display this week\'s time report')</wired-item>
+        <wired-item value="cmdLast">l (last) - display last week\'s time report</wired-item>
+        <wired-item value="cmdStretch">s (stretch) &lt;task&gt;*** - stretch last task to current time')</wired-item>
+        <wired-item value="cmdEdit">e (edit) - edit current timesheet</wired-item>
+        <wired-item value="cmdBreak">b (break) - shortcut to add break **</wired-item>
+        <wired-item value="cmdToggle">t (toggle), ?) - toggle this help text display</wired-item>`
   }
 }
 
