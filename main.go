35474734a9824d1530a057345441db462e27a772
David McPike <7037366+mcdafydd@users.noreply.github.com>
2019-05-08 15:21:11 -0500

    Changes to left+right shift hotkey

--- main.go
+++ main.go
@@ -8,45 +8,100 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/signal"
 	"runtime"
 	"sync"
 
+	hook "github.com/robotn/gohook"
 	"github.com/zserge/lorca"
 )
 
 // Go types that are bound to the UI must be thread-safe, because each binding
 // is executed in its own goroutine. In this simple case we may use atomic
 // operations, but for more complex cases one should use proper synchronization.
-type tracker struct {
+type worker struct {
 	sync.Mutex
-	cmd string
+	cmd            string
+	bounds         *lorca.Bounds
+	ui             lorca.UI
+	leftShiftDown  bool
+	rightShiftDown bool
 }
 
-func (c *tracker) RunUTT(s string) {
+// RunUTT Executes 'utt' on the command-line and prints the results
+func (c *worker) RunUTT(argv []string) {
 	c.Lock()
 	defer c.Unlock()
-	//c.count = c.count + n
+	if len(argv) == 1 {
+		cmd := exec.Command("utt", argv[0])
+		processOutput(cmd)
+	} else if len(argv) > 1 {
+		args := append([]string{"utt"}, argv...)
+		cmd := exec.Command(args[0], args[1:]...)
+		processOutput(cmd)
+	} else {
+		return
+	}
+}
+
+// Minimize Hides the application window
+// Saves the current window lorca.Bounds
+func (c *worker) Minimize() {
+	c.Lock()
+	defer c.Unlock()
+	bounds, err := c.ui.Bounds()
+	if err != nil {
+		log.Println("[ERROR] Minimize.Bounds(): ", err)
+		return
+	}
+	c.bounds = &bounds
+
+	c.bounds.WindowState = lorca.WindowStateMinimized
+	err = c.ui.SetBounds(*c.bounds)
+	if err != nil {
+		log.Println("[ERROR] Minimize.SetBounds(): ", err)
+		return
+	}
+}
+
+// Restore Restores previous visible window state after Minimize()
+func (c *worker) Restore() {
+	c.Lock()
+	defer c.Unlock()
+	bounds, err := c.ui.Bounds()
+	if err != nil {
+		log.Println("[ERROR] Minimize.Bounds(): ", err)
+		return
+	}
+	c.bounds = &bounds
+
+	c.bounds.WindowState = lorca.WindowStateNormal
+	err = c.ui.SetBounds(*c.bounds)
+	if err != nil {
+		log.Println("[ERROR] Restore.SetBounds() WindowStateNormal: ", err)
+		return
+	}
+}
+
+// processOutput executes cmd and handles any results
+func processOutput(cmd *exec.Cmd) {
+	out, err := cmd.Output()
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Printf(string(out))
+	}
+	return
 }
 
 func main() {
-	/*
-		ok := robotgo.AddEvents("ctrl", "shift", ";")
-		if ok {
-			fmt.Println("add events...")
-		}
-	*/
-	/*
-		// start hook
-		s := robotgo.Start()
-		// end hook
-		defer robotgo.End()
-	*/
 	args := []string{}
 	if runtime.GOOS == "linux" {
 		args = append(args, "--class=Lorca")
 	}
-	ui, err := lorca.New("", "", 480, 320, args...)
+
+	ui, err := lorca.New("", "", 480, 200, args...)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -58,8 +113,10 @@ func main() {
 	})
 
 	// Create and bind Go object to the UI
-	c := &tracker{}
+	c := &worker{ui: ui, cmd: ""}
 	ui.Bind("runUtt", c.RunUTT)
+	ui.Bind("minimize", c.Minimize)
+	ui.Bind("restore", c.Restore)
 
 	// Load HTML.
 	// You may also use `data:text/html,<base64>` approach to load initial HTML,
@@ -75,17 +132,85 @@ func main() {
 
 	// You may use console.log to debug your JS code, it will be printed via
 	// log.Println(). Also exceptions are printed in a similar manner.
-	ui.Eval(`
+	/*ui.Eval(`
 		console.log('Multiple values:', [1, false, {"x":5}]);
-	`)
+	`)*/
 
 	// Wait until the interrupt signal arrives or browser window is closed
 	sigc := make(chan os.Signal)
 	signal.Notify(sigc, os.Interrupt)
+
+	// start hook
+	hotkey := hook.Start()
+	// end hook
+	defer hook.End()
+
+	// main event loop
+	keepLooping := true
+	for keepLooping {
+		select {
+		case <-sigc:
+			keepLooping = false
+			break
+		case <-ui.Done():
+			keepLooping = false
+			break
+		case ev := <-hotkey:
+			if ev.Rawcode == 65505 && ev.Kind == hook.KeyDown {
+				fmt.Printf("Got left shift down = %#v\n", ev)
+				c.leftShiftDown = true
+			}
+			if ev.Rawcode == 65506 && ev.Kind == hook.KeyDown {
+				c.rightShiftDown = true
+			}
+			if ev.Rawcode == 65505 && ev.Kind == hook.KeyUp {
+				c.leftShiftDown = false
+			}
+			if ev.Rawcode == 65506 && ev.Kind == hook.KeyUp {
+				c.rightShiftDown = false
+			}
+			if c.leftShiftDown && c.rightShiftDown {
+				log.Println("Got hotkey - restoring command window")
+				c.Restore()
+			}
+		}
+	}
+
+	log.Println("exiting...")
 	select {
 	case <-sigc:
 	case <-ui.Done():
 	}
 
 	log.Println("exiting...")
+}
+
+// Maximize Brings the chrome window into visibility
+func Maximize(ui lorca.UI) {
+	bounds, err := ui.Bounds()
+	if err != nil {
+		log.Println("[ERROR] Maximize.Bounds(): ", err)
+		return
+	}
+	bounds.WindowState = lorca.WindowStateMaximized
+	err = ui.SetBounds(bounds)
+	if err != nil {
+		log.Println("[ERROR] Maximize.SetBounds(): ", err)
+		return
+	}
+}
+
+// Minimize Hides the application window
+func Minimize(ui lorca.UI) {
+	bounds, err := ui.Bounds()
+	if err != nil {
+		log.Println("[ERROR] Minimize.Bounds(): ", err)
+		return
+	}
+	bounds.WindowState = lorca.WindowStateMinimized
+	err = ui.SetBounds(bounds)
+	if err != nil {
+		log.Println("[ERROR] Minimize.SetBounds(): ", err)
+		return
+	}
 }
